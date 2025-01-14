8792d09b20f22c1cb18ccb31bccf5f2301465078
Pritam Patra <pritampatra1141@gmail.com>
2025-01-14 23:35:19 +0530

    MentorDashboard added

--- Team_Vodka/src/App.jsx
+++ Team_Vodka/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LandingPage from './Components/LandingPage'
 import PricingSection from './Components/PricingSection'
+import MentorDashboard from './Components/MentorDashBoard'
 
 const App = () => {
   return (
     <div>
       <LandingPage/>
+      <MentorDashboard/>
       <PricingSection/>
     </div>
     
